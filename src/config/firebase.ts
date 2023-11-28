1488819eeae5f280d7f2aec506495e996205eedd
orestdzhus <135337634+orestdzhus@users.noreply.github.com>
2023-11-28 10:55:08 +0200

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDSC7XkvQNvyFSutVJyFIF7G3kuyLh9LI8",
-  authDomain: "auth-app-f49dc.firebaseapp.com",
-  projectId: "auth-app-f49dc",
-  storageBucket: "auth-app-f49dc.appspot.com",
-  messagingSenderId: "680848196504",
-  appId: "1:680848196504:web:a382aa4406dc41be68e1ed",
-  measurementId: "G-YJEJ7LKJ22",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
